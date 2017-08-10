0e891d8431af99367c07b2c111c1fd198ae2ac82
Dima Piro <d1monsi10@gmail.com>
2017-08-10 03:46:25 +0300

    teest

---
+++ Weather OWM.py
@@ -0,0 +1,14 @@
+import pyowm
+
+city = input('Какой город Вас интересует?: ')
+
+owm = pyowm.OWM('35afd1462382e8b97093bb1ce1f57372', language='ru')
+
+observation = owm.weather_at_place(city)
+w = observation.get_weather()
+temperature = w.get_temperature('celsius')['temp']
+ww = w.get_detailed_status()
+
+print( 'В городе ' + city + ' ' + str(temperature) + '. В этом горде ' + ww )
+
+# https://github.com/csparpa/pyowm
